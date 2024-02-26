d18d33c74c305c7c30372ede16097b0fc0fae998
murod <murad.djanikulov@gmail.com>
2024-02-26 15:31:48 +0500

    maven task done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,16 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        for (String e : args) {
+            if (!StringUtils.isPositiveNumber(e)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ utils/src/test/java/demo/StringUtilsTest.java
@@ -0,0 +1,20 @@
+package demo;
+
+import com.epam.utils.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StringUtilsTest {
+
+    @Test
+    void testIsPositiveNumber() {
+        assertTrue(StringUtils.isPositiveNumber("10"));
+        assertTrue(StringUtils.isPositiveNumber("3.14"));
+        assertFalse(StringUtils.isPositiveNumber("-5"));
+        assertFalse(StringUtils.isPositiveNumber("abc"));
+        assertFalse(StringUtils.isPositiveNumber(""));
+        assertFalse(StringUtils.isPositiveNumber(null));
+    }
+}
